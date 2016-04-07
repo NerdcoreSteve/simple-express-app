b88cbbbf3d108fabd63a7c6554feb4bf766a99fd
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-04-06 21:43:10 -0600

    simple page

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi!')
+var express = require('express')
+var app = express()
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.send('Wazzup?')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
